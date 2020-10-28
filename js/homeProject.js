cd3b428fc48357ffbbd25902559a12cdf8330a3d
Artur <death.fire1@yandex.ru>
2020-10-28 10:19:35 +0300

    Project part 2

--- js/homeProject.js
+++ js/homeProject.js
@@ -37,12 +37,32 @@ const personalMovieDB = {
     privat : false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+     if (a != null && b != null && a != '' && b!='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+     } else {
+         console.log('error');
+         i--;
+
+     }
+     
+}
+
+if (personalMovieDB.count < 10 ) {
+    console.log('Очень мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Зорошо');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Киноман');
+} else {
+    console.log('Оішбка');
+}
 
 console.log(personalMovieDB);
+
---
+++ js/lesson14Array.js
@@ -0,0 +1,12 @@
+
+
+let num = 50;
+
+while (55 > num) {
+    console.log('1');
+    num++;
+}
+
+for (let i = 1; i < 5; i++) {
+    console.log(i);
+}
